d781af2649e7d23b1409607b6dc66b690df98dfe
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-06-10 13:14:58 -0400

    SideBar Test 2

--- staticfiles/js/menu.js
+++ staticfiles/js/menu.js
@@ -1,18 +1,30 @@
 function entrar(){
     var sidebar = document.getElementById('sidebar');
-    
     sidebar.style.display = 'block';
     var sidebaroff = document.getElementById('sidebar-off');
     sidebaroff.style.display = 'block';
+
 }
 function sair(){
     var sidebar = document.getElementById('sidebar');
     sidebar.style.display = 'none';
     var sidebaroff = document.getElementById('sidebar-off');
     sidebaroff.style.display = 'none';
+    
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    var menuBtn = document.getElementById("home-linkstyle-menu");
+    var sidebar = document.getElementById("sidebar");
+    var sidebarOff = document.getElementById("sidebar-off");
 
+    function toggleSidebar() {
+        sidebar.classList.toggle("sidebar-show");
+    }
+
+    menuBtn.addEventListener("click", toggleSidebar);
+    sidebarOff.addEventListener("click", toggleSidebar);
+  });
 
 window.onscroll = function arrowOff(){
     var arrow = document.getElementById('arrow');
