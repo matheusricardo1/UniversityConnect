7dccff562f508aa5154bb78c3f49c8bbbfcd3cdf
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-06-10 10:49:17 -0400

    Responsive FIxed

---
+++ bakp/staticfiles/UniversityConnect/js/menu.fa0e527e9d32.js
@@ -0,0 +1,33 @@
+function entrar(){
+    var sidebar = document.getElementById('sidebar');
+    
+    sidebar.style.display = 'block';
+    var sidebaroff = document.getElementById('sidebar-off');
+    sidebaroff.style.display = 'block';
+}
+function sair(){
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+    var sidebaroff = document.getElementById('sidebar-off');
+    sidebaroff.style.display = 'none';
+}
+
+
+
+window.onscroll = function arrowOff(){
+    var arrow = document.getElementById('arrow');
+    arrow.style.display ='none';
+}
+
+function checkScreenWidth() {
+    if (window.matchMedia("(min-width: 684px)").matches) {
+      document.getElementById("sidebar").style.display = "none";
+      var sidebaroff = document.getElementById('sidebar-off');
+      sidebaroff.style.display = 'none';
+    }
+}
+
+window.addEventListener("DOMContentLoaded", checkScreenWidth);
+window.addEventListener("resize", checkScreenWidth);
+
+
