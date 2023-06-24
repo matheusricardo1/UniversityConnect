2c9e291f11f28ba90a48ada1d6ea8c5c2b7d57f4
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-06-24 18:24:19 -0400

    Course Page

--- UniversityConnect/views.py
+++ UniversityConnect/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 #from models 
 
 def homepage(request):
-    return render(request, 'UniversityConnect/html/pt/index.html')
+    return render(request, 'UniversityConnect/html/pt/index.html', {'request': request,})
 
 def cursos(request):
-    return render(request, 'UniversityConnect/html/pt/cursos.html')
+    return render(request, 'UniversityConnect/html/pt/cursos.html', {'request': request,})
