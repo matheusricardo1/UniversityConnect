f76a47e3fc7315bd2cfca671edb2969bf4d0d603
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-07-26 13:05:59 -0400

    Language Button

--- static/UniversityConnect/js/menu.js
+++ static/UniversityConnect/js/menu.js
@@ -44,3 +44,57 @@ function checkScreenWidth() {
 
 window.addEventListener("DOMContentLoaded", checkScreenWidth);
 window.addEventListener("resize", checkScreenWidth);
+
+function updateLanguageURL(linkId) {
+    const selectElement = document.getElementById(linkId.replace("lang-link", "lang-options"));
+    const selectedValue = selectElement.value;
+    const langLink = document.getElementById(linkId);
+  
+    // Obter a URL atual da tag <a>
+    const currentURL = langLink.getAttribute("href");
+  
+    // Verificar se a URL contém 'en' ou 'pt-br' e alternar entre eles, exceto se já estiver em 'en'
+    const newURL = currentURL.includes("en") ? currentURL.replace("en", "pt-br") : currentURL;
+  
+    // Se a opção selecionada for 'pt-br', definir a URL com 'pt-br'
+    if (selectedValue === "pt-br") {
+      langLink.setAttribute("href", newURL);
+    } else {
+      // Caso contrário, a opção selecionada é 'en', definir a URL com 'en'
+      langLink.setAttribute("href", currentURL.replace("pt-br", "en"));
+    }
+  
+    // Redirecionar a página para a nova URL
+    window.location.href = langLink.getAttribute("href");
+  }
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+function redirectToLang() {
+    const selectElement = document.getElementById("lang-options");
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const selectedValue = selectedOption.value;
+    const urlBase = "/set_language/";
+  
+    if (selectedValue && urlBase) {
+      const url = urlBase + selectedValue;
+      const langLink = document.getElementById("lang-link");
+      langLink.setAttribute("href", url);
+     
+      setTimeout(function() {
+        window.location.href = url
+      }, 200);
+    }
+  }
+  
+
+
