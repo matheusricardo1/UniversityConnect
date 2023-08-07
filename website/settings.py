1304adf594db1d42326db55f14b396fdf2d65ff1
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-08-07 19:36:02 -0400

    Low Imgs

--- website/settings.py
+++ website/settings.py
@@ -157,7 +157,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/UniversityConnect'),
     os.path.join(BASE_DIR, "static"),
 ]
 
